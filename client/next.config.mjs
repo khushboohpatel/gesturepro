5d7435432dba20f62998230995b5a88359a51c5b
Areef Syed <areefsyed96@gmail.com>
2025-05-08 14:05:34 -0400

    Updated next.config to render Google User images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['localhost'],
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: '**',
-          port: '',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: '**',
-          port: '',
-          pathname: '**',
-        },
-      ],
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: ["localhost", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
