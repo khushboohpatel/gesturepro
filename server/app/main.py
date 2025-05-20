95ee543a2a544ac81c7ae8a6f578e76c66b4845d
eparirishit <epari.rishit@gmail.com>
2025-05-20 02:38:38 -0400

    Add CORS configuration

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import auth
 from .models.user import Base
 from .utils.db import engine
@@ -12,5 +13,13 @@ logging.basicConfig(
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
