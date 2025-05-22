b1c9f7013b4024fb2691dcbe3dd78d231dc57957
Areef Syed <areefsyed96@gmail.com>
2025-05-21 21:04:10 -0400

    Removed duplicate key for password

--- client/src/app/signup/step3.js
+++ client/src/app/signup/step3.js
@@ -44,12 +44,12 @@ export default function SignupStep3({ handleNext, step2Values }) {
       .required("This field is required!"),
     password: Yup.string()
       .min(8, "Password must be at least 8 characters")
+      .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .matches(
+        /[!@#$%^&*(),.?":{}|<>]/,
+        "Password must contain at least one special character"
+      )
       .required("This field is required!"),
-  password: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain at least one special character")
-    .required("This field is required!"),
   });
 
   const formik = useFormik({
